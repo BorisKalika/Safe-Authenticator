8cc5955031fe00fbf3de0a7d07a3540abbd8a58f
Boris Kalika <131198966+BorisKalika@users.noreply.github.com>
2023-07-20 23:20:11 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,44 +1,41 @@
-#define BLYNK_TEMPLATE_ID "TMPL4xGTbiQit"
-#define BLYNK_TEMPLATE_NAME "Safe"
-#define BLYNK_AUTH_TOKEN "nP88RDtkGIRUUoiSmtQe5XL4CdVLUJwJ"
+#define BLYNK_TEMPLATE_ID "<yourID>"
+#define BLYNK_TEMPLATE_NAME "<yourName>"
+#define BLYNK_AUTH_TOKEN "<yourToken>"
 
 #define BLYNK_PRINT Serial 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-char ssid[] = "Mr.sexobeat";
-char pass[] = "gzmg0156";
+char ssid[] = "<yourWifiName>";
+char pass[] = "<yourWifiPass>";
 
 BlynkTimer timer;
 
 void setup(){
   pinMode(4, INPUT_PULLUP);
+  pinMode(2, OUTPUT);
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
-  //timer.setInterval(2500L, notify);
+
 
 }
-
-int flag=0;
 void notify()
 {
-  int isButtonPressed = digitalRead(4);
- 
-  if (isButtonPressed==0 && flag==0) {
-    Serial.println("Unlock Request");
-     //Blynk.email("shameer50@gmail.com", "Alert", "It's Raining");
-     Blynk.logEvent("unlockrequest","Unlock Request");
-    flag=1;
-  }
-  else if (isButtonPressed==1)
-  {
-    flag=0;
-  }
-}
+   Serial.println("Unlock Request send to device");
+   Blynk.logEvent("unlockrequest","");
+ }
 
 void loop(){
   Blynk.run();
-  notify();
-  //timer.run();
+  if(digitalRead(2) == HIGH){ //Lock
+    delay(4000);
+    digitalRead(2) == LOW;
+    Serial.println("Unlock Door");
+  }
+
+  int isButtonPressed = digitalRead(4); //Notification
+  if (isButtonPressed == 0) {
+    notify();
+   }
 }
